493392b3423c4518ab2b9c7cc128292798325a3e
admin <admin@ZMIEN-MNIE>
2025-02-12 12:06:41 +0100

    Dodaj pliki projektów.

---
+++ kosci/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+
+
+class Program 
+{
+    static void Main()
+    {
+
+        Random random = new Random();
+
+        while (true) 
+        {
+            Console.WriteLine("Podaj liczbę kostek do rzucenia. (3-10)");
+            int ilosc = Convert.ToInt16(Console.ReadLine);
+            if (ilosc >2  && ilosc >0)
+            {
+
+            }
+
+        }
+
+
+
+        
+        Console.ReadLine();
+
+
+    } 
+
+}
