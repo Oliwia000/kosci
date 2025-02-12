700e8b9bd2c67f28a640b9aceae1f9884639d195
teren <teren@KOMP>
2025-02-12 19:10:34 +0100

    .

--- kosci/Program.cs
+++ kosci/Program.cs
@@ -1,31 +1,58 @@
 ﻿using System;
-using System.Diagnostics.Metrics;
+using System.Collections.Generic;
+using System.Linq;
 
-
-class Program 
+class Program
 {
     static void Main()
     {
-
         Random random = new Random();
-
-        while (true) 
+        while (true)
         {
-            Console.WriteLine("Podaj liczbę kostek do rzucenia. (3-10)");
-            int ilosc = Convert.ToInt16(Console.ReadLine);
-            if (ilosc >2  && ilosc >0)
-            {
+            int iloscKostek = PobierzLiczbeKostek();
+            List<int> rzuty = RzucKostkami(iloscKostek, random);
+            WyswietlRzuty(rzuty);
+            Console.WriteLine($"Liczba uzyskanych punktów: {ObliczPunkty(rzuty)}");
 
-            }
-
+            if (!CzyKontynuowac())
+                break;
         }
+    }
 
+    static int PobierzLiczbeKostek()
+    {
+        int ilosc;
+        do
+        {
+            Console.Write("Ile kostek chcesz rzucić? (3 - 10): ");
+        } while (!int.TryParse(Console.ReadLine(), out ilosc) || ilosc < 3 || ilosc > 10);
+        return ilosc;
+    }
 
+    static List<int> RzucKostkami(int ilosc, Random random)
+    {
+        List<int> rzuty = new List<int>();
+        for (int i = 0; i < ilosc; i++)
+            rzuty.Add(random.Next(1, 7));
+        return rzuty;
+    }
 
-        
-        Console.ReadLine();
+    static void WyswietlRzuty(List<int> rzuty)
+    {
+        for (int i = 0; i < rzuty.Count; i++)
+            Console.WriteLine($"Kostka {i + 1}: {rzuty[i]}");
+    }
 
+    static int ObliczPunkty(List<int> rzuty)
+    {
+        return rzuty.GroupBy(x => x)
+                    .Where(grupa => grupa.Count() > 1)
+                    .Sum(grupa => grupa.Sum());
+    }
 
-    } 
-
+    static bool CzyKontynuowac()
+    {
+        Console.Write("Jeszcze raz? (t/n): ");
+        return Console.ReadLine().ToLower() == "t";
+    }
 }
